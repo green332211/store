1c6cb62ec3613ae39a1200044a73e02ee50f11ee
Anatoliy Lobach <lobachtolyan123@gmail.com>
2024-08-07 18:16:39 +0300

    Added profile

--- store/users/views.py
+++ store/users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib import auth
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserProfileForm, UserRegistrationForm
 
 
 def login(request):
@@ -32,3 +32,17 @@ def registration(request):
         form = UserRegistrationForm()
     context = {"form": form}
     return render(request, "users/registration.html", context)
+
+
+def profile(request):
+    if request.method == "POST":
+        form = UserProfileForm(
+            instance=request.user, data=request.POST, files=request.FILES
+        )
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:profile"))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {"title": "Store - Профиль", "form": form}
+    return render(request, "users/profile.html", context)
